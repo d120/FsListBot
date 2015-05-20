e2e41a70320af89470b994ddf224b6f68dda7174
Max Weller <git@max-weller.de>
2015-05-20 23:19:34 +0200

    Buttons für vorheriges/nächstes Protokoll eingefügt

--- public/frame_helper.js
+++ public/frame_helper.js
@@ -1,25 +1,30 @@
 
-function last_wednesday() {
-  var time = new Date();
+function last_wednesday(start) {
+  var time = start ? start : new Date();
   do { time = new Date(+time-86400000) } while(time.getDay() != 3);
+  return time;
+}
+function next_wednesday(start) {
+  var time = start ? start : new Date();
+  do { time = new Date(+time+86400000) } while(time.getDay() != 3);
   return time;
 }
 function current_wednesday() {
   var time = new Date();
-  while(time.getDay() != 3) time = new Date(+time-86400000);
-  return time;
+  if (new Date().getDay() == 3) return time;
+  return last_wednesday(time);
 }
 function format_date_iso(date) {
   return date.getFullYear() + "-" + (date.getMonth() < 9 ? "0" : "") + (date.getMonth() + 1)
       + "-" + (date.getDate() < 9 ? "0" : "") + date.getDate();
 }
-function current_pad_url() {
+function protokoll_pad_url(date) {
   return "https://www.fachschaft.informatik.tu-darmstadt.de/pad/?group=sitzung&show=Sitzung"
-      + format_date_iso(current_wednesday()).replace(/-/g, '');
+      + format_date_iso(date ? date : current_wednesday()).replace(/-/g, '');
 }
-function rueckblick_url() {
+function protokoll_wiki_url(date) {
   return "https://www.fachschaft.informatik.tu-darmstadt.de/wiki/Fachschaftssitzung/" 
-      + format_date_iso(last_wednesday());
+      + format_date_iso(date ? date : last_wednesday());
 }
 
 function on_resize() {
